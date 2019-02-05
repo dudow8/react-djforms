3ea3cbcd8318e2e1615ab3c99b06e092928dee7e
Luann Francisco <luann.silva@wooza.com.br>
2019-02-05 18:40:12 -0200

    feat: added buildState method to FormService

--- src/lib/service/form/index.js
+++ src/lib/service/form/index.js
@@ -1,7 +1,7 @@
-export const form = ({name, ...form}) => ({
+export const form = (name, ...form) => ({
     collection: false,
     form,
-    ...name && name
+    ...name && {name}
 });
 
 export const collection = ({
@@ -27,3 +27,27 @@ export const group = ({
     label,
     orientation
 });
+
+export const buildState = (structure, state = {}) => {
+    if(!structure.collection && structure.form) {
+        const form = structure.form.reduce((form, item) => ({
+            ...buildState(item, form) 
+        }), {});
+        return structure.name ? { ...state, [structure.name]: form } : form;
+    }
+
+    if(structure.collection) {
+        return { ...state, [structure.name]: [] };
+    }
+
+    if(!structure.form) {
+        const form = structure.fields.reduce(
+            (form, item) => ({
+                ...form,
+                [item.name]: ''
+            })
+        , {});
+        return { ...state, ...form };
+    }
+    return state;
+};
